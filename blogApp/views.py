374267226a18022efd1eb839eca466ae2b28ebb0
Ahmed Serag <ahmedserag736@gmail.com>
2023-10-20 16:32:32 +0300

    Add paginator to posts for home page

--- blogApp/views.py
+++ blogApp/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from .models import Post, User
+from django.core.paginator import Paginator, EmptyPage
 
 # Home Views
 def home(request):
-    posts = Post.objects.all()
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 2)
+    page_number = request.GET.get("page", 1)
+    try:
+        posts = paginator.page(page_number)
+    except:
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'home.html', {'posts': posts})
 
 # Single Post Views
